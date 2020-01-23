052b2db12e62f458dfd424a13ce4bce1ad598cc1
giraldokarol <karol.giraldo9505@gmail.com>
2020-01-23 11:36:09 +0100

    Create folder for images/Products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { ProfilComponent } from './profil/profil.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 
 
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     HomeComponent,
     AuthentificationComponent,
-    ProfilComponent
+    ProfilComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
