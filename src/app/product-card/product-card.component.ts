6f62df9add2b93d5bb08a01ac9d195d48191bb34
giraldokarol <karol.giraldo9505@gmail.com>
2020-01-23 13:07:18 +0100

    Add elements to Profil.html

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../entities/product.model';
 
 @Component({
   selector: 'app-product-card',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
+  @Input() product :Product;
+  
   constructor() { }
 
   ngOnInit() {
