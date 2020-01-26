e4ef1a2c05e971cb6eae0a4526e073afa96fd20f
giraldokarol <karol.giraldo9505@gmail.com>
2020-01-26 22:32:42 +0100

    Styles CSS Section Product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ServiceProductService } from '../services/service-product.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../entities/product.model';
+import { ServiceGetCategoryService } from '../services/service-get-category.service';
+import { Category } from '../entities/category.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-product',
@@ -11,11 +14,16 @@ import { Product } from '../entities/product.model';
 export class ProductComponent implements OnInit {
   
   param:string;
+  nameCategory:string;
+  nameProfile:string;
 
   pro: Product;
+  category :Category[];
 
-  constructor(private product : ServiceProductService, private route : ActivatedRoute) {
-    this.param=route.snapshot.params['id'];
+  constructor(private product : ServiceProductService, private route : ActivatedRoute,
+              private categories : ServiceGetCategoryService, private cookie : CookieService,
+              private router: Router) {
+              this.param=route.snapshot.params['id'];
    }
 
   ngOnInit() {
@@ -24,6 +32,19 @@ export class ProductComponent implements OnInit {
     this.product.getProduct(id).subscribe(pro =>{
       this.pro=pro;
     });
-  }
+
+    //Get Category Name
+    this.categories.getAllCategories().subscribe( category => {
+      this.category=category;
+    }); 
+    
+    //Get cookie name
+    this.nameProfile=this.cookie.get('name');
 
 }
+
+clickBack(){
+  this.router.navigate(['/profile/'+this.nameProfile]);
+}
+
+}  
